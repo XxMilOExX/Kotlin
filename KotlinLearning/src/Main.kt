30a15fbc6b65f274cd72d986fd2f669c7244035f
miloe <miloegrich@gmail.com>
2024-03-10 13:33:14 +0300

    Hello world

--- KotlinLearning/src/Main.kt
+++ KotlinLearning/src/Main.kt
@@ -1,14 +1,44 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.nio.file.attribute.AclEntry.Builder
+import kotlin.reflect.typeOf
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    var breakFlag = true;
+    while(breakFlag) {
+        println("Hi, how many people do you want to watch a vacation for?")
+        val participants = readln().toInt();
+        if (participants <= 0) {
+            println("You entered an incorrect value")
+        }
+        println("How much money do you have in $")
+        var price = readln().toInt();
+        if (price < 0) {
+            price = 0;
+        }
+        for (i in 0..5) {
+            val client = HttpClient.newBuilder().build();
+            val Api = ApiCreate(participants, price, client)
+            println(Api?.body())
+        }
+
+        println("Do you want to continue (yes or not)")
+
+        breakFlag = readln() == "yes"
     }
 }
+
+fun ApiCreate(participants: Int, price: Int, client: HttpClient): HttpResponse<String>? {
+    val uri = URI.create("http://www.boredapi.com/api/activity?participants=${participants}&price=${price}")
+    val request = HttpRequest.newBuilder().uri(uri).build();
+    return client.send(request, HttpResponse.BodyHandlers.ofString());
+
+}
+
+
